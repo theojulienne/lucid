6cf9c1a090b1b9e88c06a19361c25b7b28e4c8cf
Cody Lodrige <cody@lodrige.com>
2007-12-18 07:44:39 +0000

    First commit of new dispatch code. This will remove libsigc++ thankfully...

---
+++ trunk/trials/ldispatchlist.cpp
@@ -0,0 +1,126 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "FastDelegate.h"
+
+typedef fastdelegate::FastDelegate<void (void *)> LDelegate;
+
+class LDispatchList
+{
+protected:
+    LDispatchList();
+public:
+    ~LDispatchList();
+    static LDispatchList * Add(LDispatchList * list, LDelegate func, void * user_data, 
+        void (* val_free_fn) (void *));
+    static LDispatchList * Remove(LDispatchList * list, LDispatchList * link);
+    static void Invoke(LDispatchList * list);
+    static void Destroy(LDispatchList * list);
+private:
+    LDispatchList * m_next;
+    LDelegate m_func;
+    void * m_user_data;
+    void (* m_val_free_fn) (void *);  
+
+    static LDispatchList * FindPrevious(LDispatchList * list, LDispatchList * link);              
+};
+
+inline LDispatchList::LDispatchList(): m_next(NULL), m_func(NULL), m_user_data(NULL), m_val_free_fn(NULL)
+{      
+}    
+
+inline LDispatchList::~LDispatchList()
+{    
+    if(this->m_val_free_fn)
+        this->m_val_free_fn(this->m_user_data);
+}
+
+inline LDispatchList * LDispatchList::Add(LDispatchList * list, LDelegate func, void * user_data, 
+        void (* val_free_fn) (void *))
+{
+    LDispatchList * new_list = new LDispatchList();
+
+    new_list->m_func = func;
+    new_list->m_user_data = user_data;
+    new_list->m_val_free_fn = val_free_fn;
+    new_list->m_next = list;
+
+    return new_list;
+}
+
+inline LDispatchList * LDispatchList::FindPrevious(LDispatchList * list, LDispatchList * link)
+{
+    LDispatchList * prev = NULL;
+	
+    while (list) 
+    {
+        if (list == link)
+			break;
+		prev = list;
+		list = list->m_next;
+	}
+
+	return prev;
+}
+
+inline LDispatchList * LDispatchList::Remove(LDispatchList * list, LDispatchList * link)
+{
+    LDispatchList * prev = FindPrevious(list, link);
+	LDispatchList * current = prev ? prev->m_next : list;
+
+	if (current) 
+    {
+		if (prev)
+			prev->m_next = current->m_next;
+		else
+			list = current->m_next;
+		
+        current->m_next = NULL;
+	}
+
+    delete link;
+	
+    return list;
+}
+
+inline void LDispatchList::Invoke(LDispatchList * list)
+{
+    while(list)
+    {
+        list->m_func(list->m_user_data);
+        list = list->m_next;
+    }                       
+}
+
+inline void LDispatchList::Destroy(LDispatchList * list)
+{
+    while(list)
+    {
+        LDispatchList * next = list->m_next;
+        delete list;
+        list = next;
+    }     
+}
+
+static void foo(void * p)
+{
+    printf("%s(%p)\n", __FUNCTION__, p);
+}
+
+static void foo_del(void * p)
+{
+    printf("%s(%p)\n", __FUNCTION__, p);
+}
+
+int main(int argc, char ** argv)
+{
+    printf("%d\n", sizeof(LDispatchList));
+    LDispatchList * list = NULL, * elem = NULL;
+    list = LDispatchList::Add(list, LDelegate(foo), (void *)0xdeadbeef, foo_del);
+    elem = list;
+    list = LDispatchList::Add(list, LDelegate(foo), (void *)0xc0ffee, foo_del);
+    list = LDispatchList::Remove(list, (LDispatchList *)elem);    
+    LDispatchList::Invoke(list);
+    LDispatchList::Destroy(list);
+    return 0;
+}
+
