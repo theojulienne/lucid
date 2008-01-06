b831f15f50dea711d0ea135176e61a0eb3486b78
Theo Julienne <theo.julienne@gmail.com>
2008-01-06 06:41:56 +0000

    better testing

--- trunk/trials/swigtest/vtest.cpp
+++ trunk/trials/swigtest/vtest.cpp
@@ -2,13 +2,45 @@
 
 #include "vtest.hpp"
 
-void Foo::bar( int a, int b )
+void Foo::boo( int a, int b )
 {
-	printf( "Foo::bar(%d,%d) called in C++\n", a, b );
+	printf( "Foo::boo(%d,%d) called in C++\n", a, b );
 }
+
+void Foo::bee( int a, int b )
+{
+	printf( "Foo::bee(%d,%d) called in C++\n", a, b );
+}
+
 
 void baz( Foo *f, int a, int b )
 {
 	f->bar( a, b );
+	f->boo( a, b );
+	f->bee( a, b );
 }
 
+
+// not public:
+class CFoot: public Foo
+{
+	void bee( int a, int b )
+	{
+		printf( "CFoot::bee(%d,%d) called in C++\n", a, b );
+	}
+	
+	void boo( int a, int b )
+	{
+		printf( "CFoot::boo(%d,%d) called in C++\n", a, b );
+	}
+	
+	void bar( int a, int b )
+	{
+		printf( "CFoot::bar(%d,%d) called in C++\n", a, b );
+	}
+};
+
+Foo *makeAFoo( )
+{
+	return new CFoot;
+}
--- trunk/trials/swigtest/vtest.hpp
+++ trunk/trials/swigtest/vtest.hpp
@@ -1,8 +1,11 @@
 class Foo
 {
 public:
-	virtual void bar( int a, int b );
+	void bee( int a, int b );
+	virtual void boo( int a, int b );
+	virtual void bar( int a, int b )=0;
 };
 
 void baz( Foo *f, int a, int b );
 
+Foo *makeAFoo( );
